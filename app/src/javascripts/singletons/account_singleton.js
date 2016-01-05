72ef618d0ce447d0e23b49f1e8d101b81977915b
Daniel <danrpts@gmail.com>
2016-01-04 19:54:59 -0800

    renamed entities to singeltons

---
+++ app/src/javascripts/singletons/account_singleton.js
@@ -0,0 +1,4 @@
+var _ = require('underscore');
+var account = require('../models/account_Model.js');
+
+module.exports =  new account.Model();
---
+++ app/src/javascripts/singletons/tasks_singleton.js
@@ -0,0 +1,4 @@
+var _ = require('underscore');
+var tasks = require('../models/tasks_Collection.js');
+
+module.exports = new tasks.Collection();
