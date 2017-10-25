a2939afac6c6d678acfa6f92897581fe6dfbed64
Daniel <danrpts@gmail.com>
2017-10-25 09:47:04 -0700

    prettier was ran

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,26 @@
-var gulp = require('gulp');
-var glob = require('glob');
-var plugins = require('gulp-load-plugins')();
-var argv = require('minimist')(process.argv.slice(2));
+var gulp = require("gulp");
+var glob = require("glob");
+var plugins = require("gulp-load-plugins")();
+var argv = require("minimist")(process.argv.slice(2));
 
 // Setup all tasks
 var client = argv.client || argv.c;
 var env = argv.env || argv.e;
-var list = argv.tasks || argv.t || (!!argv._.length) ? argv._.join(',') : '*';
-var pattern = './gulp_tasks/{'+ list +',}.js'; // notice the comma
+var list = argv.tasks || argv.t || !!argv._.length ? argv._.join(",") : "*";
+var pattern = "./gulp_tasks/{" + list + ",}.js"; // notice the comma
 var tasks = glob.sync(pattern); // must be synchronous
 
 // A config object for all tasks
 var config = {
-  input: './webclient/' + client,
-  output: './webserver/public/',
-  libs: 'jquery jquery-ui-browserify human-time underscore backbone backbone.localstorage'.split(' '),
-  templates: '.html .ejs .tpl .tmpl .glsl'.split(' '),
-  fonts: 'eot svg ttf woff woff2 otf'.split(' '),
+  input: "./webclient/" + client,
+  output: "./webserver/public/",
+  libs: "jquery jquery-ui-browserify human-time underscore backbone backbone.localstorage".split(
+    " "
+  ),
+  templates: ".html .ejs .tpl .tmpl .glsl".split(" "),
+  fonts: "eot svg ttf woff woff2 otf".split(" "),
   defaults: []
-}
+};
 
 // Development or Production
 /*
@@ -29,9 +31,9 @@ set default
 */
 
 // Load all tasks
-tasks.forEach(function (path) {
+tasks.forEach(function(path) {
   require(path)(plugins, config);
 });
 
 // Run default tasks
-gulp.task('default', config.defaults);
+gulp.task("default", config.defaults);
